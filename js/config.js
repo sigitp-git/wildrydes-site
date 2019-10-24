10b0f9668443c3bf1b09dffc55fb9e77ce3a73f6
sigitpr <sigitp@amazon.com>
2019-10-24 11:25:07 -0500

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_swx8QYhwJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '68oimr751mgrp0i5l3vraueh7h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
